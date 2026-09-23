4ad2e69625663009eefa3b84eba1de058de25f87
Marcus Melo <mpaulobr@gmail.com>
2026-09-23 19:09:05 -0300

    feat: create folder structure and dtos

---
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/dto/OrderProduct.java
@@ -0,0 +1,16 @@
+package br.com.microservices.orchestrated.orchestratorservice.core.dto;
+
+import br.com.microservices.orchestrated.orderservice.core.document.Product;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderProduct {
+
+    private Product product;
+    private int quantity;
+
+}
